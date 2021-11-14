02e8c82ad851a1efde42f58151b79117210e5a32
WML <mlwdesign2020@gmail.com>
2021-11-15 00:10:24 +0800

    新增框架-header下拉選單、右側工具列(內容未完成)

--- prototype/html/js/layout.js
+++ prototype/html/js/layout.js
@@ -1,6 +1,5 @@
 var $canvas = document.getElementById("canvas");
 var $menu = document.getElementById("left_menu");
-var $aside = document.getElementById("aside_func");
 var $cont = document.getElementById("right_content");
 
 // menu-show/hide //
@@ -14,20 +13,28 @@ function menuToggle() {
     }
 }
 
-function asideToggle() {
-    if ($aside.classList) {
-        $aside.classList.toggle("aside-open");
-        $aside.style.display = 'block';
+// aside-menu-show/hide //
+var divs = ["aside_func1", "aside_func2", "aside_func3", "aside_func4", "aside_func5"];
+var visibleDivId = null;
+
+
+function asideToggle(divId) {
+    if (visibleDivId === divId) {
+        //visibleDivId = null;
+    } else {
+        visibleDivId = divId;
+    }
+    var i, divId, div;
+    for (i = 0; i < divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if (visibleDivId === divId && div.style.display === "none") {
+            div.style.display = "block";
+        } else {
+            div.style.display = "none";
+        }
     }
 }
-
-$('.aside-btn').click(function() {
-    $(this).toggleClass('aside-active');
-});
-
-$('.menu-btn a').click(function() {
-    $(this).toggleClass('menu-active');
-});
 
 // bootstrap-tooltip //
 $(document).ready(function() {
@@ -35,22 +42,7 @@ $(document).ready(function() {
         $(this).attr('data-toggle', 'tooltip');
         $(this).attr('data-placement', 'left');
     });
-    $('[data-toggle="tooltip"]').tooltip();
-});
-
-// card-detail-show/hide //
-// $('.detail-title').each(function() {
-//     $(this).click(function() {
-//         $(this).closest('.card-detail').find('.detail-board').stop(true, true).slideToggle();
-//         $(this).toggleClass('detail-title-close');
-//     });
-// });
-
-// table-detail-col-focus //
-$('.table-col-detail').each(function() {
-    $(this).click(function() {
-        $(this).css('background', '#F6EFE6');
-        $(this).closest('.table-col').siblings().find('.table-col-detail').css('background', 'none');
+    $('[data-toggle="tooltip"]').tooltip({
+        trigger : 'hover'
     });
 });
-
